8fad00238fe6731d854a8257fb037c5a131ea4f3
kbarkowski <karol.barkowski@gmail.com>
2016-09-14 00:34:38 +0200

    new solutions added

---
+++ ConsoleApplication1/Algorithms/Implementation/DivisibleSumPairs/DivisibleSumPairs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader("Algorithms\\Implementation\\DivisibleSumPairs\\samples\\testCase0.txt"))
+        {
+            int[] line = Array.ConvertAll(sr.ReadLine().Split(' '), Int32.Parse);
+            int[] arr = Array.ConvertAll(sr.ReadLine().Split(' '), Int32.Parse);
+            int k = line[1];
+
+            int total = 0;
+            for (int j = 1; j < arr.Length; j++)
+            {
+                for (int i = 0; i < j; i++)
+                {
+                    if ((arr[i] + arr[j]) % k == 0)
+                        total++;
+                }
+            }
+
+            Console.WriteLine(total.ToString());
+        }
+        Console.ReadLine();
+    }
+}
---
+++ ConsoleApplication1/Algorithms/Implementation/JumpingOnTheClouds/JumpingOnTheClouds.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader("Algorithms\\Implementation\\JumpingOnTheClouds\\samples\\testCase1.txt"))
+        {
+            sr.ReadLine();
+            int[] arr = Array.ConvertAll(sr.ReadLine().Split(' '), Int32.Parse);
+
+            int playerAt = 0;
+            int jumps = 0;
+            while (playerAt != arr.Length - 1)
+            {
+                if (arr.Length - 1 - playerAt == 1) //one to go
+                    playerAt++;
+                else if (arr[playerAt + 1] == 1 || (arr[playerAt + 1] == 0 && arr[playerAt + 2] == 0)) //next cloud is thundercloud or we got two clear clouds ahead
+                    playerAt += 2;
+                else if (arr[playerAt + 2] == 1) //thundercloud is second from here
+                    playerAt += 1;
+
+                jumps++;
+            }
+            Console.WriteLine(jumps.ToString());
+        }
+        Console.ReadLine();
+    }
+}

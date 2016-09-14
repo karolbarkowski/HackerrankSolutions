7b6bf76363d737f386ca1cc5ca0cc866276b98d0
kbarkowski <karol.barkowski@gmail.com>
2016-09-15 00:50:40 +0200

    no message

---
+++ ConsoleApplication1/Algorithms/Implementation/Kangaroo/Kangaroo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    //private static int nwd(int a, int b)
+    //{
+    //    int x = a, y = b;
+    //    while (x != y)
+    //    {
+    //        if (x > y)
+    //            x = x - y;
+    //        else
+    //            y = y - x;
+    //    }
+    //    return x;
+    //}
+
+    //private static int nww(int a, int b)
+    //{
+    //    return a * b / nwd(a, b);
+    //}
+
+    static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader("Algorithms\\Implementation\\Kangaroo\\samples\\testCase22.txt"))
+        {
+            int[] tokens_x1 = Array.ConvertAll(sr.ReadLine().Split(' '), Int32.Parse);
+            double x1 = tokens_x1[0];
+            double v1 = tokens_x1[1];
+            double x2 = tokens_x1[2];
+            double v2 = tokens_x1[3];
+
+            if (v1 == v2 || (x1 < x2 && v1 < v2) || (x1 > x2 && v1 > v2) || (x1 == x2 && v1 != v2))
+                Console.WriteLine("NO");
+            else
+            {
+                double n = (x1 - x2) / (v2 - v1);
+                bool isPositiveInt = n > 0 && n % 1 == 0;
+                Console.WriteLine(isPositiveInt ? "YES" : "NO");
+            }
+        }
+        Console.ReadLine();
+    }
+}
---
+++ ConsoleApplication1/Algorithms/Implementation/RepeatedString/RepeatedString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader("Algorithms\\Implementation\\RepeatedString\\samples\\testCase1.txt"))
+        {
+            string s = sr.ReadLine();
+            decimal n = long.Parse(sr.ReadLine());
+
+            decimal timesSFits = Math.Floor(n / s.Length);
+            decimal r = n % s.Length;
+
+            //calculate for initial string
+            decimal l1 = s.Length - s.Replace("a", "").Length;
+
+            //calculate for remaining part of string
+            string sRest = s.Substring(0, (int)r);
+            decimal l2 = sRest.Length - sRest.Replace("a", "").Length;
+
+            Console.WriteLine(l1 * timesSFits + l2);
+        }
+        Console.ReadLine();
+    }
+}

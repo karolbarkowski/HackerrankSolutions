d7b0d8b0d812857e77efc5d042a477d87e4c7b66
kbarkowski <karol.barkowski@gmail.com>
2016-09-15 21:12:13 +0200

    no message

---
+++ ConsoleApplication1/Algorithms/Implementation/CutTheSticks/CutTheSticks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader("Algorithms\\Implementation\\CutTheSticks\\samples\\testCase0.txt"))
+        {
+            sr.ReadLine();
+            int[] sticks = Array.ConvertAll(sr.ReadLine().Split(' '), Int32.Parse);
+
+            int count = sticks.Where(c => c > 0).Count();
+
+            do
+            {
+                Console.WriteLine(count);
+
+                int min = sticks.Where(c => c > 0).Min();
+                for (int i = 0; i < sticks.Length; i++)
+                {
+                    sticks[i] = Math.Max(0, sticks[i] - min);
+                }
+
+                count = sticks.Where(c => c > 0).Count();
+            }
+            while (count > 0);
+
+        }
+        Console.ReadLine();
+    }
+}
---
+++ ConsoleApplication1/Algorithms/Implementation/ServiceLane/ServiceLane.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader("Algorithms\\Implementation\\ServiceLane\\samples\\testCase0.txt"))
+        {
+            int[] nt = Array.ConvertAll(sr.ReadLine().Split(' '), Int32.Parse);
+            int N = nt[0];
+            int T = nt[1];
+            int[] width = Array.ConvertAll(sr.ReadLine().Split(' '), Int32.Parse);
+            
+
+            for (int t = 0; t < T; t++)
+            {
+                int[] ij = Array.ConvertAll(sr.ReadLine().Split(' '), Int32.Parse);
+                int i = ij[0];
+                int j = ij[1];
+
+                int min = 4;
+                for (int x = i; x <= j; x++)
+                {
+                    if (width[x] < min)
+                        min = width[x];
+                }
+
+                Console.WriteLine(min);
+            }
+        }
+        Console.ReadLine();
+    }
+}

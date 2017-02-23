83270df864550cbf5fd35a73a2e394dd2182547a
kbarkowski <karol.barkowski@gmail.com>
2017-02-23 03:17:19 +0100

    new tasks

---
+++ ConsoleApplication1/Algorithms/Strings/CamelCase/CamelCase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader("Algorithms\\Strings\\CamelCase\\samples\\testCase0.txt"))
+        {
+            string s = sr.ReadLine();
+            int counter = 1;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (Char.IsUpper(s[i])) counter++;
+            }
+
+            Console.WriteLine(counter.ToString());
+        }
+        Console.ReadLine();
+    }
+}
---
+++ ConsoleApplication1/Algorithms/Strings/Pangrams/Pangrams.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader("Algorithms\\Strings\\Pangrams\\samples\\testCase0.txt"))
+        {
+            string s = sr.ReadLine();
+
+            Regex regex = new Regex(@"(?=.*a)(?=.*b)(?=.*c)(?=.*d)(?=.*e)(?=.*f)(?=.*g)(?=.*h)(?=.*i)(?=.*j)(?=.*k)(?=.*l)(?=.*m)(?=.*n)(?=.*o)(?=.*p)(?=.*q)(?=.*r)(?=.*s)(?=.*t)(?=.*u)(?=.*v)(?=.*w)(?=.*x)(?=.*y)(?=.*z)", RegexOptions.IgnoreCase);
+            MatchCollection matches = regex.Matches(s);
+
+            Console.WriteLine(matches.Count > 0 ? "pangram" : "not pangram");
+        }
+        Console.ReadLine();
+    }
+}
+
+

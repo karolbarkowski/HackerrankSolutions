7a99c2850975576654df0533cd2033b410974af8
kbarkowski <karol.barkowski@gmail.com>
2017-02-24 11:04:08 +0100

    new added

---
+++ ConsoleApplication1/Algorithms/Strings/AlternatingCharacters/AlternatingCharacters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader("Algorithms\\Strings\\AlternatingCharacters\\samples\\testCase0.txt"))
+        {
+            int N = int.Parse(sr.ReadLine());
+            
+            for (int i = 0; i < N; i++)
+            {
+                int deletions = 0;
+                string S = sr.ReadLine();
+                for (int j = 0; j < S.Length - 1; j++)
+                {
+                    if (S[j] == S[j + 1]) deletions++;
+                }
+
+                Console.WriteLine(deletions);
+            }
+
+        }
+        Console.ReadLine();
+    }
+}
+
+

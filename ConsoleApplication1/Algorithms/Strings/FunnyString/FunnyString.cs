9909c4109617175f702b81a09f402ed662660745
kbarkowski <karol.barkowski@gmail.com>
2017-02-24 03:15:04 +0100

    new added

---
+++ ConsoleApplication1/Algorithms/Strings/FunnyString/FunnyString.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+class Solution
+{
+    private static string reverseString(string input)
+    {
+        char[] chars = input.ToCharArray();
+        Array.Reverse(chars);
+        return new string(chars);
+    }
+
+    static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader("Algorithms\\Strings\\FunnyString\\samples\\testCase0.txt"))
+        {
+            int T = int.Parse(sr.ReadLine());
+
+            for (int i = 0; i < T; i++)
+            {
+                string S = sr.ReadLine();
+                string R = reverseString(S);
+
+                bool eq = true;
+                for (int j = 0; j < S.Length - 2; j++)
+                {
+                    int Si = (int)S[j];
+                    int Si2 = (int)S[j + 1];
+
+                    int Ri = (int)R[j];
+                    int Ri2 = (int)R[j + 1];
+
+                    eq = Math.Abs(Si - Si2) == Math.Abs(Ri - Ri2);
+                    if (!eq)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine(eq ? "Funny" : "Not Funny");
+            }
+
+        }
+        Console.ReadLine();
+    }
+}
+
+

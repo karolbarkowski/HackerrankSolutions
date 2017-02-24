e1109d8950f091799e173a974fa7d3a757afb531
kbarkowski <karol.barkowski@gmail.com>
2017-02-24 03:18:59 +0100

    perf updated

--- ConsoleApplication1/Algorithms/Strings/FunnyString/FunnyString.cs
+++ ConsoleApplication1/Algorithms/Strings/FunnyString/FunnyString.cs
@@ -3,13 +3,6 @@ using System.IO;
 
 class Solution
 {
-    private static string reverseString(string input)
-    {
-        char[] chars = input.ToCharArray();
-        Array.Reverse(chars);
-        return new string(chars);
-    }
-
     static void Main(String[] args)
     {
         using (StreamReader sr = new StreamReader("Algorithms\\Strings\\FunnyString\\samples\\testCase0.txt"))
@@ -19,7 +12,6 @@ class Solution
             for (int i = 0; i < T; i++)
             {
                 string S = sr.ReadLine();
-                string R = reverseString(S);
 
                 bool eq = true;
                 for (int j = 0; j < S.Length - 2; j++)
@@ -27,8 +19,8 @@ class Solution
                     int Si = (int)S[j];
                     int Si2 = (int)S[j + 1];
 
-                    int Ri = (int)R[j];
-                    int Ri2 = (int)R[j + 1];
+                    int Ri = (int)S[S.Length - 1 - j];
+                    int Ri2 = (int)S[S.Length - 2 - j];
 
                     eq = Math.Abs(Si - Si2) == Math.Abs(Ri - Ri2);
                     if (!eq)

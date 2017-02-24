63d47ea6093bce4379d9f498a4458729480f1430
kbarkowski <karol.barkowski@gmail.com>
2017-02-24 02:56:53 +0100

    new added

---
+++ ConsoleApplication1/Algorithms/Strings/MarsExploration/MarsExploration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader("Algorithms\\Strings\\MarsExploration\\samples\\testCase9.txt"))
+        {
+            string s = sr.ReadLine();
+
+            int count = 0;
+            for (int i = 0; i < s.Length; i += 3)
+            {
+                if (s[i] != 'S') count++;
+                if (s[i + 1] != 'O') count++;
+                if (s[i + 2] != 'S') count++;
+            }
+
+            Console.WriteLine(count);
+        }
+        Console.ReadLine();
+    }
+}
+
+

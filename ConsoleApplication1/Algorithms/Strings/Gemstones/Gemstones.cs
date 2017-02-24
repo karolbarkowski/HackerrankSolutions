939ea3a8707f481e19d7b7cfc0370deae19b37f2
kbarkowski <karol.barkowski@gmail.com>
2017-02-24 10:51:37 +0100

    new added

---
+++ ConsoleApplication1/Algorithms/Strings/Gemstones/Gemstones.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader("Algorithms\\Strings\\Gemstones\\samples\\testCase0.txt"))
+        {
+            int N = int.Parse(sr.ReadLine());
+            Hashtable gems = new Hashtable();
+
+            //read first line
+            string firstLine = sr.ReadLine();
+            foreach (char c in firstLine)
+            {
+                gems[c] = true;
+            }
+
+            //read remaining lines
+            for (int i = 0; i < N - 1; i++)
+            {
+                string S = sr.ReadLine();
+                foreach (char c in firstLine)
+                {
+                    if (!S.Contains(c.ToString()))
+                    {
+                        gems.Remove(c);
+                    }
+                }
+            }
+
+            Console.WriteLine(gems.Count);
+        }
+        Console.ReadLine();
+    }
+}
+
+

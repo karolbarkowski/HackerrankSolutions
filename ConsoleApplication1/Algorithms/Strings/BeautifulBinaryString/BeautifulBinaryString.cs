78800e23b336c93be2b70139e913fe7f5f28e6c1
kbarkowski <karol.barkowski@gmail.com>
2017-02-24 11:13:17 +0100

    new added

---
+++ ConsoleApplication1/Algorithms/Strings/BeautifulBinaryString/BeautifulBinaryString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader("Algorithms\\Strings\\BeautifulBinaryString\\samples\\testCase0.txt"))
+        {
+            sr.ReadLine();
+            string S = sr.ReadLine();
+
+            Regex regex = new Regex("010");
+            var matches = regex.Matches(S);
+
+            Console.WriteLine(matches.Count);
+        }
+        Console.ReadLine();
+    }
+}
+
+

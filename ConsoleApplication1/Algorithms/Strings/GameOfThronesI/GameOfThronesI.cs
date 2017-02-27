b37347b5a0c806174e21d62744fe0677608fe6e2
kbarkowski <karol.barkowski@gmail.com>
2017-02-27 11:30:33 +0100

    new added

---
+++ ConsoleApplication1/Algorithms/Strings/GameOfThronesI/GameOfThronesI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader("Algorithms\\Strings\\GameOfThronesI\\samples\\testCase1.txt"))
+        {
+            string s = sr.ReadLine();
+
+            Hashtable ht = new Hashtable(s.Length);
+            int oddCount = 0;
+            foreach (char c in s)
+            {
+                //toggle between true/false values to indicate if given letter occured odd or even number of times
+                if (ht.ContainsKey(c))
+                {
+                    ht[c] = !(bool)ht[c];
+                    if ((bool)ht[c]) oddCount--;
+                    if (!(bool)ht[c]) oddCount++;
+                }
+                else {
+                    ht[c] = false;
+                    oddCount++;
+                }
+            }
+
+            Console.WriteLine(oddCount <= 1 ? "YES" : "NO");
+        }
+        Console.ReadLine();
+    }
+}
+
+
---
+++ ConsoleApplication1/Algorithms/Strings/StringConstruction/StringConstruction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader("Algorithms\\Strings\\StringConstruction\\samples\\testCase0.txt"))
+        {
+            int N = int.Parse(sr.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(sr.ReadLine().ToCharArray().Distinct().Count());
+            }
+        }
+        Console.ReadLine();
+    }
+}
+
+
